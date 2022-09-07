76c6fccbfb521b75d3c174f56b27b0d51eb6400a
carterj <cdjeurissen@bsu.edu>
2022-09-07 18:10:07 -0400

    Very basic while loop begun for the calculator logic

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,9 @@
 public class Calculator {
+    public static void main(String args[]){
+        int stage = 1;
+
+        while (stage == 1) {
+            //if button 0 pressed, number1 = 0;
+        }
+    }
 }
