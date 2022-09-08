c621821310aa8e852197f43fde4ee45bae1eb526
carterj <cdjeurissen@bsu.edu>
2022-09-08 17:11:33 -0400

    Developed the skeleton of the CalculatorMath class

--- src/main/java/edu/bsu/cs/CalculatorMath.java
+++ src/main/java/edu/bsu/cs/CalculatorMath.java
@@ -5,7 +5,19 @@ public class CalculatorMath {
         int stage = 1;
 
         while (stage == 1) {
-            //if button 0 pressed, number1 = 0;
+            //if button 0 pressed, number1 = 0, and stage = 2;
+        }
+
+        while (stage == 2) {
+            //if button + pressed, symbol = plus, and stage = 3;
+        }
+
+        while (stage == 3){
+            //if button 0 pressed, number2 = 0, and stage = 4;
+        }
+
+        while (stage == 4){
+            //if button = pressed { if symbol = plus, value = number1 + number2, and stage = 1;
         }
     }
 }
