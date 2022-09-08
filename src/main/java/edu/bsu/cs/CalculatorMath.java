378da024d7e002083554ee932d24081ab8e5d6d1
carterj <cdjeurissen@bsu.edu>
2022-09-08 17:32:35 -0400

    Fixed description of the future code in the CalculatorMath

--- src/main/java/edu/bsu/cs/CalculatorMath.java
+++ src/main/java/edu/bsu/cs/CalculatorMath.java
@@ -1,24 +1,24 @@
 package edu.bsu.cs;
 
-public class CalculatorMath {
+public class CalculatorMath extends App {
     public static void main(String args[]){
         int stage = 1;
 
         while (stage == 1) {
-            //if button 0 pressed, number1 = 0, and stage = 2;
+            //if # button pressed, add to string
+            //if symbol button pressed, save variable with its function...
+            //...take written number and convert to decimal, and save as variable, and stage = 2
+            //(unless the sqrt or square, then just provide result there)
+
         }
 
         while (stage == 2) {
-            //if button + pressed, symbol = plus, and stage = 3;
+            //if # button pressed, add to new string
+            //if equal button pressed, convert previous number to decimal...
+            //then apply the two numbers based on saved symbol, and convert answer to quaternary
+            //then, return that, and stage = 1
         }
 
-        while (stage == 3){
-            //if button 0 pressed, number2 = 0, and stage = 4;
-        }
-
-        while (stage == 4){
-            //if button = pressed { if symbol = plus, value = number1 + number2, and stage = 1;
-        }
     }
 }
 
