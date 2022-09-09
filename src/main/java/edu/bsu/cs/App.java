1d65d64ed42eac24b42ec0d7f745fbba05eb0736
Wesley <wjkring@bsu.edu>
2022-09-09 10:14:48 -0400

    Restructure App class for future implimentation of buttons

--- src/main/java/edu/bsu/cs/App.java
+++ src/main/java/edu/bsu/cs/App.java
@@ -9,26 +9,59 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    private Button zero;
+    private Button one;
+    private Button two;
+    private Button three;
+    private Button add;
+    private Button subtract;
+    private Button multiply;
+    private Button divide;
 
+    public App() {
+        setButtonLabels();
+        setButtonEvents();
+    }
+
+    @Override
     public void start(Stage primaryStage) {
         primaryStage.setScene(new Scene(setCalculatorLayout()));
         primaryStage.setTitle("Quaternary Calculator");
         primaryStage.show();
     }
 
+    private void setButtonEvents(){
+        zero.setOnAction(event -> zeroTask());
+    }
+
+    private void setButtonLabels(){
+        zero = new Button("0");
+        one = new Button("1");
+        two = new Button("2");
+        three = new Button("3");
+        add = new Button("+");
+        subtract = new Button("-");
+        multiply = new Button("*");
+        divide = new Button("/");
+    }
+
     private Parent setCalculatorLayout() {
         VBox calculatorUI = new VBox();
         GridPane calculatorGrid = new GridPane();
-        calculatorGrid.add(new Button("3"), 0, 0);
-        calculatorGrid.add(new Button("2"), 1, 0);
-        calculatorGrid.add(new Button("1"), 0, 1);
-        calculatorGrid.add(new Button("0"), 1, 1);
-        calculatorGrid.add(new Button("x"), 2, 0);
-        calculatorGrid.add(new Button("/"), 2, 1);
-        calculatorGrid.add(new Button("+"), 2, 2);
-        calculatorGrid.add(new Button("-"), 2, 3);
+        calculatorGrid.add(three, 0, 0);
+        calculatorGrid.add(two, 1, 0);
+        calculatorGrid.add(one, 0, 1);
+        calculatorGrid.add(zero, 1, 1);
+        calculatorGrid.add(multiply, 2, 0);
+        calculatorGrid.add(divide, 2, 1);
+        calculatorGrid.add(add, 2, 2);
+        calculatorGrid.add(subtract, 2, 3);
         calculatorUI.getChildren().add(calculatorGrid);
         return calculatorUI;
+    }
+
+    private void zeroTask(){
+        System.out.println("0");
     }
 }
 
