e9a8ef1595b90abd5fd78c0643ec3cc569f84759
Wesley <wjkring@bsu.edu>
2022-09-11 18:11:14 -0400

    Delete unused import statements

--- src/main/java/edu/bsu/cs/App.java
+++ src/main/java/edu/bsu/cs/App.java
@@ -7,15 +7,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
-import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
 
